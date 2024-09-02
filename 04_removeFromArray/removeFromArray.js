439adcae7b2ec05e45ab15cd273370147949ea23
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 15:32:06 -0400

    Implement removeFromArray method to remove arbitrary element(s) from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...removeThese) {
+    for( let rt of removeThese){
+      arr = arr.filter( x => x!==rt )
+    }
+    return arr;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
