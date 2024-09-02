da9573e10f60412a37c423a8fb8c298988c78feb
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 15:44:50 -0400

    Update removeFromArray code with an alternative approach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,25 @@
-const removeFromArray = function(arr, ...removeThese) {
+/* original attempt, which works great */
+const removeFromArray_original = function(arr, ...removeThese) {
     for( let rt of removeThese){
       arr = arr.filter( x => x!==rt )
     }
     return arr;
   };
+
+/* implementing something like the simpler solution */
+const removeFromArray_solution = function(arr, ...removeThese) {
+    newArr = [];
+    arr.forEach( x => {
+        if (!removeThese.includes(x)){
+            newArr.push(x);
+        }
+    })
+    return newArr;
+  };
+
+// either one works..
+const removeFromArray = removeFromArray_original;
+//const removeFromArray = removeFromArray_solution;
 
 // Do not edit below this line
 module.exports = removeFromArray;
