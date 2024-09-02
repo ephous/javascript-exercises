dec9d3589153e31c5ec9eaee7b022a5d57f932c7
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 15:00:50 -0400

    Implement repeatString based on a loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(theString, numRepeats) {
+    
+    if (numRepeats < 0){
+        return 'ERROR';
+    }
 
+    let i = 0;
+    let returnString = '';
+    while( i < numRepeats) {
+        returnString += theString;
+        i++;
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
