45ba373793d220600b41d2d4ed12d14ad3a34e21
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 15:14:11 -0400

    Implement reverseString in JavaScript

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,26 @@
-const reverseString = function() {
+/* this works */
+const reverseString_1 = function(inStr) {
+    
+    for(const x of inStr){
+        arr.push(x);
+    }
+    
+    let newStr = '';
+    for (let i=arr.length-1;i>=0;i--){
+        newStr = newStr + arr[i];
+    }
 
+    return newStr;
+
+};
+
+/* also works */
+const reverseString = function(inStr) {
+    let newStr = '';
+    for(let i = inStr.length-1; i >= 0; i--) {
+        newStr = newStr + inStr[i];
+    }        
+    return newStr;
 };
 
 // Do not edit below this line
