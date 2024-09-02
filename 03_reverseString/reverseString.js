40074dec917137208d81091e0bc43be404f4a73e
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 15:16:27 -0400

    Update reverseString per (simpler) solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -15,13 +15,19 @@ const reverseString_1 = function(inStr) {
 };
 
 /* also works */
-const reverseString = function(inStr) {
+const reverseString_2 = function(inStr) {
     let newStr = '';
     for(let i = inStr.length-1; i >= 0; i--) {
         newStr = newStr + inStr[i];
     }        
     return newStr;
 };
+
+/* solution */
+// did not realize we could use String functions for this exercise - doh!
+const reverseString = function (string) {
+    return string.split("").reverse().join("");
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
