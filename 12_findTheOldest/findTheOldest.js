8e9451f22fc93c760f40aa0ff687725c827d63d0
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 23:38:21 -0400

    Implement routine to find the oldest person from a list of birth/death dates.
    This draft implementation did not use reduce, but
    it probably should/could have.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+/* 
+Given an array of objects representing people with a birth 
+and death year, return the oldest person.
+*/
 
+const findTheOldest = function(arr) {
+    
+    let oldestAge=-1;
+    let oldestIdx=-1;
+    arr.forEach( (x,currIdx) => {
+        
+        let xAge;
+        if (x.hasOwnProperty('yearOfDeath')){
+            xAge = x.yearOfDeath - x.yearOfBirth;
+        } else {
+            const rightNow = new Date(Date.now());
+            xAge = rightNow.getFullYear() - x.yearOfBirth;
+        }
+
+        if (xAge > oldestAge){
+            oldestAge = xAge;
+            oldestIdx = currIdx;
+        }
+    });
+
+    return arr[oldestIdx];
+    
 };
 
 // Do not edit below this line
