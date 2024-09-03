f8cc6cc21b8567d74c6b11702918a7a58850a37c
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 20:56:06 -0400

    Implement sumAll routine per requirement

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,37 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    //----------------------------------
+    // ensure Number, per requirement
+    if (typeof a != 'number'){
+        return('ERROR');
+    }
+    if (typeof b != 'number'){
+        return('ERROR');
+    }
+
+    //----------------------------------
+    // ensure positive, per requirement
+    if ((a < 0) || (b < 0)){
+        return('ERROR');
+    }
+
+    //----------------------------------
+    // ensure integers, per requirement
+    if (a != Math.trunc(a)){
+        return('ERROR');
+    }
+    if (b != Math.trunc(b)){
+        return('ERROR');
+    }
+
+    //----------------------------------
+    let summ = 0;
+    let [starts,stops] = (b > a) ? [a,b] : [b,a]
+    for(let i=starts;i<=stops;i++){
+        summ+=i;
+    }
+
+    return summ;
 
 };
 
