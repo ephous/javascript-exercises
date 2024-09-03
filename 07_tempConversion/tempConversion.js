afd33fbbd4baf9465871b388a9c1451cd767736c
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 21:45:13 -0400

    Implement temperature-conversion routines.
    Added F-to-C and C-to-F routines. Both routines
    return the converted temprature, rounded to the
    one decimal place if the result is not an integer.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+// NOTE: number.toFixed(1) always returns a decimal, 
+// even if it is 0; eg, 0.0 rather than 0
+
+const convertToCelsius = function(tempF) {
+  let result = 5 * (tempF - 32)/9;
+  return Math.round(10 * result)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  result = 9 * tempC / 5 + 32;
+  return Math.round(10 * result)/10;
 };
+
+//console.log( convertToCelsius(32));
 
 // Do not edit below this line
 module.exports = {
