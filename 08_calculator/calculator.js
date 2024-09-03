6c07f1d769c2c74bec3cdb81205ba49cd5f458b5
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 22:01:23 -0400

    Implement calculator, per requirements

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(vals) {
+	let summ=0;
+  for (let i = 0; i < vals.length; i++){
+    summ+=vals[i];
+  }
+  return summ;
 };
 
-const multiply = function() {
-
+const multiply = function(vals) {
+	let product=1;
+  for (let i = 0; i < vals.length; i++){
+    product*=vals[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a==0) {return 1;}
+  if (a==1) {return a;}
+  return a*factorial(a-1);
 };
+
+console.log( factorial(5));
 
 // Do not edit below this line
 module.exports = {
