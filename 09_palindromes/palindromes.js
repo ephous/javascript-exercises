b75958ab089eede19c05e9290a1810650a050bac
ephous <179286206+ephous@users.noreply.github.com>
2024-09-02 22:22:20 -0400

    Implement routine to determine if a given string is a palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,38 @@
-const palindromes = function () {
+/* palindrome is string that spelled the same both forwards 
+and backwards, usually w/o considering punctuation or word breaks
+
+*/
+
+const palindromes = function (inStr) {
+    
+    original_string_condensed = inStr.split(/\W/g).join('').toLowerCase();
+
+    // split into array of w+ords, in reverse order
+    let newArr = []
+    for(x of inStr.split(/\W/g).reverse() ){
+        newArr.push( x.split('').reverse().join('') )
+    }
+    
+    new_string_condensed = newArr.join('').toLowerCase();
+    
+    //console.log(original_string_condensed);
+    //console.log(new_string_condensed)
+    
+    return original_string_condensed===new_string_condensed;
+
 
 };
+
+/*
+tests = ["A car, a man, a maraca.", 
+         "Rats live on no evil star.",
+         "Lid off a daffodil.",
+         "Animal loots foliated detail of stool lamina.",
+         "A nut for a jar of tuna." ];
+for( const x of tests){
+    console.log( palindromes(x) );
+}
+*/
 
 // Do not edit below this line
 module.exports = palindromes;
